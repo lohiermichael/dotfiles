494bb564248d4164d6e75fb4c96412d32b7b60f9
Michael Lohier <lohiermichael@gmail.com>
2022-10-14 10:12:58 +0200

    add a mac_settings.sh file to allow keeping pressed
    Add in the command to allow keeping pressed for not repeating but
    proposing accents instead
    
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

---
+++ mac/mac_settings.sh
@@ -0,0 +1,7 @@
+################################################################################
+# Mac specif settings
+# Note: it requires logout and log back in to take effects
+################################################################################
+
+# Enable key press accents
+defaults write -g ApplePressAndHoldEnabled -bool true
