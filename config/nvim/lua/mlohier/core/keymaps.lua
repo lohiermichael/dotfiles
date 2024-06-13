863ac02248867d192e389adf9241f873d6eee7de
Michael Lohier <lohiermichael@gmail.com>
2024-06-13 17:08:19 +0200

    neovim: add command Copywright: <leader>c
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- config/nvim/lua/mlohier/core/keymaps.lua
+++ config/nvim/lua/mlohier/core/keymaps.lua
@@ -64,3 +64,41 @@ keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 -- list available help tags
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>"
 )
+
+----------------------
+-- specific
+----------------------
+
+-- add a commented copyright line at the top of the buffer
+function add_copyright()
+  -- get the current buffer's file type
+  local filetype = vim.bo.filetype
+  local copyright_line
+
+  -- building the line
+  -- determine the header line based on file type
+  if filetype == "lua" then
+      copyright_line = "--"
+  elseif filetype == "python" then
+      copyright_line = "#"
+  elseif filetype == 'javascript' then
+      copyright_line = "//"
+  else
+      copyright_line = ""
+  end
+
+  copyright_line = copyright_line .. " Copyright ©"
+  copyright_line = copyright_line .. " Michael Lohier"
+  copyright_line = copyright_line .. " " .. os.date("%Y")
+  copyright_line = copyright_line .. " All rights reserved."
+
+  vim.api.nvim_buf_set_lines(0, 0, 0, false, {copyright_line})
+  -- save the file after adding the line
+  vim.api.nvim_command('write')
+end
+
+-- map a key to call the function (Example: <Leader>c)
+vim.api.nvim_set_keymap(
+  'n', '<Leader>c', '<Cmd>lua add_copyright()<CR>',
+  { noremap = true, silent = true }
+)
