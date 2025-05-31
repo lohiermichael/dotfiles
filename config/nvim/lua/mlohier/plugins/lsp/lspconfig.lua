b37378080819cd14cfe36ff428541de6d911a6ec
Michael Lohier <lohiermichael@gmail.com>
2025-05-31 20:11:56 +0200

    neovim: remove unused variable
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- config/nvim/lua/mlohier/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/mlohier/plugins/lsp/lspconfig.lua
@@ -13,7 +13,7 @@ end
 local keymap = vim.keymap -- for conciseness
 
 -- enable keybinds only for when lsp server available
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   -- keybind options
   local opts = { noremap = true, silent = true, buffer = bufnr }
 
